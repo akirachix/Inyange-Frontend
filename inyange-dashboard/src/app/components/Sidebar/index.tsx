3b080028c55dd2263413f0565bbd4a004943b879
eshe-aziz <eshehaledhaziz@gmail.com>
2024-09-24 12:09:31 +0300

    add sidebar

--- inyange-dashboard/src/app/components/Sidebar/index.tsx
+++ inyange-dashboard/src/app/components/Sidebar/index.tsx
@@ -1,12 +1,91 @@
+// import React, { useState } from 'react';
+// import Image from 'next/image';
+// import Link from 'next/link';
+// import { Home, ShoppingCart, ClipboardList, CreditCard, LogOut } from 'lucide-react';
+
+// const Sidebar = () => {
+//   const [activeLink, setActiveLink] = useState('');
+
+//   const handleLinkClick = (link: string) => {
+//     setActiveLink(link);
+//   };
+
+//   return (
+//     <div className="flex flex-col bg-[#263C5A] h-screen p-6 md:p-12 lg:p-14 space-y-10 md:space-y-14 w-64 md:w-72 lg:w-80">
+//       <div className="flex items-center space-x-2 mb-6 md:mb-8">
+//         <Image
+//           src="/images/bmlogo.png"
+//           alt="BuildMart Logo"
+//           width={200}
+//           height={200}
+//         />
+//       </div>
+
+   
+//       <nav className="flex flex-col space-y-10 text-lg md:text-xl font-bold">
+//         <Link 
+//           href="/" 
+//           className={`flex items-center space-x-4 ${activeLink === '/' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
+//           onClick={() => handleLinkClick('/')}
+//         >
+//           <Home className="w-8 h-8 md:w-10 md:h-10" />
+//           <span>Dashboard</span>
+//         </Link>
+
+//         <Link 
+//           href="components/Inventory" 
+//           className={`flex items-center space-x-4 ${activeLink === '/Inventory' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
+//           onClick={() => handleLinkClick('/Inventory')}
+//         >
+//           <ShoppingCart className="w-8 h-8 md:w-10 md:h-10" />
+//           <span>Inventory</span>
+//         </Link>
+
+//         <Link 
+//           href="/orders" 
+//           className={`flex items-center space-x-4 ${activeLink === '/orders' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
+//           onClick={() => handleLinkClick('/orders')}
+//         >
+//           <ClipboardList className="w-8 h-8 md:w-10 md:h-10" />
+//           <span>Orders</span>
+//         </Link>
+
+//         <Link 
+//           href="/payments" 
+//           className={`flex items-center space-x-4 ${activeLink === '/payments' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
+//           onClick={() => handleLinkClick('/payments')}
+//         >
+//           <CreditCard className="w-8 h-8 md:w-10 md:h-10" />
+//           <span>Payments</span>
+//         </Link>
+
+//         <Link 
+//           href="/logout" 
+//           className={`flex items-center space-x-4 ${activeLink === '/logout' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
+//           onClick={() => handleLinkClick('/logout')}
+//         >
+//           <LogOut className="w-8 h-8 md:w-10 md:h-10" />
+//           <span>Logout</span>
+//         </Link>
+//       </nav>
+//     </div>
+//   );
+// };
+
+// export default Sidebar;
+
+
+
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { Home, ShoppingCart, ClipboardList, CreditCard, LogOut } from 'lucide-react';
+import { LuHome, LuClipboardList, LuCreditCard } from "react-icons/lu";
+import { FiShoppingCart, FiLogOut } from "react-icons/fi";
 
 const Sidebar = () => {
   const [activeLink, setActiveLink] = useState('');
 
-  const handleLinkClick = (link: string) => {
+  const handleLinkClick = (link) => {
     setActiveLink(link);
   };
 
@@ -16,28 +95,27 @@ const Sidebar = () => {
         <Image
           src="/images/bmlogo.png"
           alt="BuildMart Logo"
-          width={160}
-          height={160}
+          width={200}
+          height={200}
         />
       </div>
 
-   
-      <nav className="flex flex-col space-y-14 text-lg md:text-xl font-bold">
+      <nav className="flex flex-col space-y-10 text-lg md:text-xl font-bold">
         <Link 
           href="/" 
           className={`flex items-center space-x-4 ${activeLink === '/' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
           onClick={() => handleLinkClick('/')}
         >
-          <Home className="w-8 h-8 md:w-10 md:h-10" />
+          <LuHome className="w-8 h-8 md:w-10 md:h-10" />
           <span>Dashboard</span>
         </Link>
 
         <Link 
-          href="/inventory" 
-          className={`flex items-center space-x-4 ${activeLink === '/inventory' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
-          onClick={() => handleLinkClick('/inventory')}
+          href="components/Inventory" 
+          className={`flex items-center space-x-4 ${activeLink === '/Inventory' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
+          onClick={() => handleLinkClick('/Inventory')}
         >
-          <ShoppingCart className="w-8 h-8 md:w-10 md:h-10" />
+          <FiShoppingCart className="w-8 h-8 md:w-10 md:h-10" />
           <span>Inventory</span>
         </Link>
 
@@ -46,7 +124,7 @@ const Sidebar = () => {
           className={`flex items-center space-x-4 ${activeLink === '/orders' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
           onClick={() => handleLinkClick('/orders')}
         >
-          <ClipboardList className="w-8 h-8 md:w-10 md:h-10" />
+          <LuClipboardList className="w-8 h-8 md:w-10 md:h-10" />
           <span>Orders</span>
         </Link>
 
@@ -55,7 +133,7 @@ const Sidebar = () => {
           className={`flex items-center space-x-4 ${activeLink === '/payments' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
           onClick={() => handleLinkClick('/payments')}
         >
-          <CreditCard className="w-8 h-8 md:w-10 md:h-10" />
+          <LuCreditCard className="w-8 h-8 md:w-10 md:h-10" />
           <span>Payments</span>
         </Link>
 
@@ -64,7 +142,7 @@ const Sidebar = () => {
           className={`flex items-center space-x-4 ${activeLink === '/logout' ? 'text-[#F8B612]' : 'text-white'} hover:text-[#F8B612] transition-all duration-300`} 
           onClick={() => handleLinkClick('/logout')}
         >
-          <LogOut className="w-8 h-8 md:w-10 md:h-10" />
+          <FiLogOut className="w-8 h-8 md:w-10 md:h-10" />
           <span>Logout</span>
         </Link>
       </nav>
