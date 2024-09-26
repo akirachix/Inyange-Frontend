534ebea62f468477b2e01c580d726c9342c649d7
eshe-aziz <eshehaledhaziz@gmail.com>
2024-09-26 13:34:31 +0300

    add sidebar

--- inyange-dashboard/src/app/components/Layout/index.tsx
+++ inyange-dashboard/src/app/components/Layout/index.tsx
@@ -8,7 +8,7 @@ import Sidebar from '../Sidebar';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="flex min-h-screen">
-      <div className="w-64 bg-gray-800 text-white">
+      <div className="w-64">
         <Sidebar />
       </div>
       <div className="flex-grow p-4">
