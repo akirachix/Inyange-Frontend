de68acad96d6d8cf219609bf57e41919e9305331
rehema_funani <rehemafunani@gmail.com>
2024-10-16 17:19:38 +0300

    made changes

--- inyange-dashboard/src/app/(pwa)/pwa/utils/types.ts
+++ inyange-dashboard/src/app/(pwa)/pwa/utils/types.ts
@@ -1,16 +1,15 @@
 export interface Credentials{
-    email:string;
-    password:string;
+  email:string;
+  password:string;
 }
 export interface MaterialData {
-    material_id: number;
-    material_name: string;
-    brand_name: string;
-    category_name: string;
-    description: string;
-    quantity: number;
-    price: number;
-    image: File | null;
-    category: string; 
-  }
-  
+  material_id: number;
+  material_name: string;
+  brand_name: string;
+  category_name: string;
+  description: string;
+  quantity: number;
+  price: number;
+  image: File | null;
+  category: string; 
+}
